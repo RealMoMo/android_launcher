1a77cf93696dc91928591f6b9af9f27298dd6280
炜烨 <momo_weiye@126.com>
2017-04-11 09:58:30 +0800

    CustomDialogActivity
    增加注释说明各参数的含义

--- customdialogactivity/src/main/java/com/example/customdialogactivity/CustomDialogActivity.java
+++ customdialogactivity/src/main/java/com/example/customdialogactivity/CustomDialogActivity.java
@@ -2,6 +2,7 @@ package com.example.customdialogactivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.View;
@@ -18,21 +19,27 @@ public class CustomDialogActivity extends Activity {
 
         WindowManager m = getWindowManager();
         Display d = m.getDefaultDisplay();  //为获取屏幕宽、高
-
+        //另一种获取屏幕宽高的像素
+//        int height = getResources().getDisplayMetrics().heightPixels;
+//        int width = getResources().getDisplayMetrics().widthPixels;
         WindowManager.LayoutParams p = getWindow().getAttributes();  //获取对话框当前的参数值
+
+
         p.height = (int) (d.getHeight() * 0.5);   //高度设置为屏幕的0.5
         p.width = (int) (d.getWidth() * 0.7);    //宽度设置为屏幕的0.7
+        Log.d("realmo","after height:"+p.height+",width:"+p.width);
+
+        //以下2个属性可以不设置，直接用系统默认的
         p.alpha = 1.0f;      //设置本身透明度
-        p.dimAmount = 0.0f;      //设置黑暗度
+        p.dimAmount = 0.0f;      //设置黑暗度(即非DialogActivity周围区域的颜色)
 
         getWindow().setAttributes(p);     //设置生效
         getWindow().setGravity(Gravity.RIGHT);       //设置靠右对齐
     }
 
 
-
     public void touchTest(View view) {
-        Toast.makeText(this,"TouchTextView",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "TouchTextView", Toast.LENGTH_SHORT).show();
         this.finish();
     }
 }
